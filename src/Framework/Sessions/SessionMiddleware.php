31e34c0907e52df7a81c8d93a2c402dacd841809
Edwin <edwin.dayot@sfr.fr>
2017-10-25 15:40:42 +0200

    Min PHP version updated

---
+++ src/Framework/Sessions/SessionMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * SessionManager.php
+ *
+ * @author   Edwin Dayot <edwin@greenpandagames.com>
+ */
+
+namespace Shorty\Framework\Sessions;
+
+
+/**
+ * Class SessionManager
+ *
+ * @package Shorty\Framework\Sessions
+ */
+class SessionManager
+{
+
+}
